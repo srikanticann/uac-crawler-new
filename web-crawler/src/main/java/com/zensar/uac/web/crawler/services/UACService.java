ab85c3163a595cbefe0a9c54887f141b43d5427d
unknown <SB48380@ICANW42SLPC1246.ind.zensar.com>
2016-08-29 17:02:15 +0530

    code formatted

--- web-crawler/src/main/java/com/zensar/uac/web/crawler/services/UACService.java
+++ web-crawler/src/main/java/com/zensar/uac/web/crawler/services/UACService.java
@@ -2,12 +2,12 @@ package com.zensar.uac.web.crawler.services;
 
 /**
  * @author Sagar Balai
- * UACService is responsible to create crawling report for new URL or already processed URL's.
- * This is basic interface and exposed to external user and will act as starting point for this web crawler application.
+ *         UACService is responsible to create crawling report for new URL or already processed URL's.
+ *         This is basic interface and exposed to external user and will act as starting point for this web crawler application.
  */
-public interface UACService 
-{
+public interface UACService {
 
-	void crawl(String URL, String emailID);
-	void generateReport();
+    void crawl(String URL, String emailID);
+
+    void generateReport();
 }
